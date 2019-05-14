467c03c648a4fa0ef0108a34f7a11bc568b81efb
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-14 17:19:08 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -49,22 +49,7 @@ def run
   help 
   puts "Please enter a command:"
   command_input = gets.strip
-  until (command_input = gets.strip) = "exit" 
-   if (command_input = gets.strip) == "list"
-     list
-   end
-   if (command_input = gets.strip) == "play"
-       play(songs)
-   end
-   if (command_input = gets.strip) == "help"
-     help
-   end
-   if (command_input = gets.strip) == "exit"
-     exit_jukebox
-   end
-   break
- end
-end
   
+end 
   
 
