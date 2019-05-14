2f786fcf25afeb0edfa7bc185a7a7796e6e854af
Abril Anchondo Reynaga <abril79reynaga@gmail.com>
2019-05-14 16:27:42 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -29,6 +29,13 @@ end
 
 def play(songs)
   puts "Please enter a song name or number:"
-  
+  user_input = gets.chomp
+  songs.each do |song|
+    if song == user_input
+      puts "Playing #{song}"
+    else
+      puts "Invalid input, please try again"
+    end
+  end
 end
 
